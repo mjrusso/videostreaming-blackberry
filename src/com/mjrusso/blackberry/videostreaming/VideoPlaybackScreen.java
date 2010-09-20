97db2c191c6658dc9789b8509ebe04f72b1ed239
Michael Russo <mjrusso@gmail.com>
2010-09-20 01:22:24 -0400

    remove unused imports

--- src/com/mjrusso/blackberry/videostreaming/VideoPlaybackScreen.java
+++ src/com/mjrusso/blackberry/videostreaming/VideoPlaybackScreen.java
@@ -3,7 +3,6 @@ package com.mjrusso.blackberry.videostreaming;
 import java.io.*;
 import javax.microedition.media.*;
 import javax.microedition.media.control.*;
-import net.rim.blackberry.api.browser.Browser;
 import net.rim.device.api.system.*;
 import net.rim.device.api.ui.*;
 import net.rim.device.api.ui.component.*;
